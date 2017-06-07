33cd64f794e790c4f50ad0c62f1aa085b596c80f
Tomas Guillen <trguillen@gmail.com>
2017-06-07 10:07:23 -0400

    More Image Advances

---
+++ SteelcaseVQI/Models/MainPageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SteelcaseVQI.Models
+{
+    public class MainPageModel
+    {
+        public Assembly Assembly { get; set; }
+        public Component Component { get; set; }
+
+    }
+}
